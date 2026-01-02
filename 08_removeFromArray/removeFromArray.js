beab807a7cc50066ce3a6c2e73299785e6ca86f8
IBilldThings <200523655+IBilldThings@users.noreply.github.com>
2026-01-01 20:51:54 -0500

    feat: add removeFromArray code

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...values) {
+  return array.filter(item => !values.includes(item));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
