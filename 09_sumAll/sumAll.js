c2e6da0baee2a5b1c4363b5175624829dd7df623
IBilldThings <200523655+IBilldThings@users.noreply.github.com>
2026-01-02 18:01:45 -0500

    feat: add sumAll code

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+    if (intOne < 0 || intTwo < 0){
+        return "ERROR"
+    }
+    if (typeof intOne != "number" && Number.isNaN(intOne)) {
+        return "ERROR"
+    }
+    if (typeof intTwo != "number" && Number.isNaN(intTwo)) {
+        return "ERROR"
+    }
+    if (!Number.isInteger(intOne) || !Number.isInteger(intTwo)){
+        return "ERROR"
+    }
+    if (intOne != Math.round(intOne) || intTwo != Math.round(intTwo)){
+        return "ERROR"
+    }
 
+    let sum = 0;
+
+    let smaller = intOne;
+    let larger = intTwo;
+    if (intOne > intTwo){ //Set the end number to whichever is larger.
+        larger = intOne;
+        smaller = intTwo; //Set smaller to intTwo if it is smaller.
+    }
+
+    for (i = 0; i <= larger; i++){
+        if (i >= smaller){
+            sum += i;
+        }
+    }
+    return sum
 };
 
 // Do not edit below this line
