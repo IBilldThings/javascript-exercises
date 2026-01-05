af6965fafa8bb8b231b119a4d56ac369413a8b44
IBilldThings <200523655+IBilldThings@users.noreply.github.com>
2026-01-04 19:39:55 -0500

    feat: finish exercise 12 calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a , b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a , b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((sum, current) => sum * current, 1);
 };
 
-const power = function() {
-	
-};
+const power = function(a, b) {
+  return a ** b;
+}
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+	for (let i = 1; i <= number; i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
