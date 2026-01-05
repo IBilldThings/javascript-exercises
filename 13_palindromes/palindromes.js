3129a212c72664324d156611f5e185bd8b191068
IBilldThings <200523655+IBilldThings@users.noreply.github.com>
2026-01-04 20:27:19 -0500

    feat: finish exercise 13 palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let reversed = '';
+    const newText = text.replace(/[^a-z0-9]/gi, '').toLowerCase();
 
+    for (let i = newText.length - 1; i >= 0; i--){
+        reversed += newText[i];
+    }
+
+    return reversed === newText;
 };
 
 // Do not edit below this line
