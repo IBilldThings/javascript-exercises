6fe1174faa647b71a69ef2bc534ac4d509d87d86
IBilldThings <200523655+IBilldThings@users.noreply.github.com>
2026-01-04 21:31:56 -0500

    feat: finish exercise 14 fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let iteration = number;
+    if (typeof number === "string"){
+        iteration = Number(number);
+    }
 
+    if (number < 0){
+        return "OOPS"
+    }
+
+    let current = 1;
+    let previous = 0;
+    let next = 0;
+
+    for (let i = 0; i < iteration; i++){
+        next = current + previous;
+        previous = current;
+        current = next;
+    }
+    return previous;
 };
 
 // Do not edit below this line
